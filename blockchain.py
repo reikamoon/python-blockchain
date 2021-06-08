a8e796b186114d344eaf1e40ce7974fbb5712531
Anjelica <ambrosio.anjelica@gmail.com>
2021-06-08 15:54:49 -0700

    finished blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,34 +1,107 @@
 # Python Blockchain
+import hashlib
+import json
+from time import time
 
 class Blockchain(object):
 
-    def __init__(self):
+    def __init__(self):
+       
+        self.chain = []
+        self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
 
+    def proof_of_work(self, last_proof):
 
-       self.chain = []
+        """This method is where you the consensus algorithm is implemented.
 
-       self.current_transactions = []
+        It takes two parameters including self and last_proof"""
 
-    def new_block(self):
+        proof = 0
 
-           #This function creates new blocks and then adds to the existing chain
+        while self.valid_proof(last_proof, proof) is False:
 
-        pass
+            proof +=1
 
-    def new_transaction(self):
+        return proof
 
-           #This function adds a new transaction to already existing transactions
+    @staticmethod
 
-        pass
+    def valid_proof(last_proof, proof):
 
+            """This method validates the block"""
 
-   def hash(block):
+            guess = f'{last_proof}{proof}'.encode()
 
-           #Used for hashing a block
+            guess_hash = hashlib.sha256(guess).hexigest()
 
+            return guess_hash[:4] == "0000"
 
-   def last_block(self):
+    def new_block(self, proof, previous_hash=None):
 
-           # Calls and returns the last block of the chain
+            #This function creates new blocks and then adds to the existing chain
 
-        pass
+            """This method will contain two parameters proof, previous hash"""
+
+            block = {
+
+                'index': len(self.chain) + 1,
+
+                'timestamp' : time(),
+
+                'proof': proof,
+
+                previous_hash: previous_hash or self.hash(self.chain[-1]),
+
+            }
+
+            # Set the current transaction list to empty.
+
+            self.current_transactions=[]
+
+            self.chain.append(block)
+
+            return block
+
+    def new_transaction(self):
+
+            #This function adds a new transaction to already existing transactions
+
+            """This will create a new transaction which will be sent to the next block. It will contain
+
+            three variables including sender, recipient and amount
+
+            """
+
+            self.current_transactions.append(
+
+                {
+
+                    'sender': sender,
+
+                    'recipient': recipient,
+
+                    'amount': amount,
+
+                }
+
+            )
+            return self.last_block['index'] + 1
+
+    @staticmethod
+
+    def hash(block):
+            #Used for hashing a block
+
+            """The follow code will create a SHA-256 block hash and also ensure that the dictionary is ordered"""
+
+            block_string = json.dumps(block, sort_keys=True).encode()
+
+            return hashlib.sha256(block_string).hexdigest()
+
+    @property
+
+    def last_block(self):
+            # Calls and returns the last block of the chain
+
+            return self.chain[-1]
