11c678b0bf32f5249090aec38db088c453fa400e
Anjelica <ambrosio.anjelica@gmail.com>
2021-06-08 15:18:08 -0700

    first commit

---
+++ blockchain.py
@@ -0,0 +1,34 @@
+# Python Blockchain
+
+class Blockchain(object):
+
+    def __init__(self):
+
+
+       self.chain = []
+
+       self.current_transactions = []
+
+    def new_block(self):
+
+           #This function creates new blocks and then adds to the existing chain
+
+        pass
+
+    def new_transaction(self):
+
+           #This function adds a new transaction to already existing transactions
+
+        pass
+
+
+   def hash(block):
+
+           #Used for hashing a block
+
+
+   def last_block(self):
+
+           # Calls and returns the last block of the chain
+
+        pass
